545b3877e34df4ab891d30574f673f4126e18cad
bobby3003 <roberto3003@riseup.net>
2021-09-20 05:10:25 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
-    let number = 3;
+    let number = 7;
     if number < 5 {
         println!("condition was true");
     } else {
         println!("condition was false");
     }
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+    let num = 6;
+    if num % 4 == 0 {
+        println!("num is divisible by 4");
+    } else if num % 3 == 0 {
+        println!("num is divisible by 3");
+    } else if num % 2 == 0 {
+        println!("num is divisible by 2");
+    } else {
+        println!("num is not divisible by 4, 3, or 2");
+    }
+    let condition = true;
+    let num2 = if condition { 5 } else { 6 };
+    println!("The value of num2 is: {}", num2);
 }
